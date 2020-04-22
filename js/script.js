9a2fa0fd5735c43b25b56b9e07a45cbede7328b5
Eugene <misuna99@gmail.com>
2020-04-22 12:32:46 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+a = ("5");
+console.log("a");
+
+let something;
+console.log(something);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+
+
+let arr = ['plum.png','orange.jpg','aplle.bmp'];
+
+console.log(arr[0])
+
+//alert("Hello world");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+
+//let answer = +prompt("Есть ли вам 18?", "Да");
+
+//console.log(typeof(answer));
+
+//console.log("arr" + "- obgect");
+
+//console.log("4" + " - obgect");
+
+let incr = 10,
+    decr = 10;
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log("2" == 2);
+
+    let isChecked = false,
+        isClose = false;
+
+    console.log(isChecked || !isClose); 
